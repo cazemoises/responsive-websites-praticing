0b20a3dc61540aad212b19e60aa6fa2146bbd75d
cazemoises <moises.cazee@gmail.com>
2022-12-06 18:38:23 -0300

    s

---
+++ medicenter/assets/js/script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    document.querySelector(".menu_mobile").addEventListener("click", () => {
+    if (document.querySelector(".menu nav ul").style.display == "flex") {
+        document.querySelector(".menu nav ul").style.display = "none"
+    }
+    else {
+        document.querySelector(".menu nav ul").style.display = "flex"
+    }
+})
+}
